5c9ada732385b87a4c74f98e78ca2e127ddc40a8
unknown <ryder.george20@gmail.com>
2023-02-28 13:48:33 +0000

    added questions and getting user answers

--- index.js
+++ index.js
@@ -4,9 +4,57 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const Userquestions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the title for your project?',
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Description of project:'
+        },
+        {
+            type: 'input',
+            name: 'projectInstallation',
+            message: 'Project Installation',
+            default: 'node ./index.js'
+        },
+        {
+            type: 'input',
+            name: 'projectUsage',
+            message: 'Usage for project?'
+        },
+        {
+            type: 'list',
+            name: 'projectLicense',
+            message: 'Licences used?',
+            choices: ['MIT', 'APACHE', 'The Unlicense', 'None']
+        },
+        {
+            type: 'input',
+            name: 'projectContributors',
+            message: 'Were there any contributors?',
+            default: 'none'
+        },
+        {
+            type: 'input',
+            name: 'projectTests',
+            message: 'What tests need to be done?',
+            default: 'node ./index.js'
+        },
+        {
+            type: 'input',
+            name: 'projectQuestions',
+            message: 'Questions?'
+        }
 
-];
+    ]).then(function(answer){
+        generateMarkdown(answer);
+    });
+    
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -14,7 +62,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    Userquestions()        
 }
 
 // function call to initialize program
